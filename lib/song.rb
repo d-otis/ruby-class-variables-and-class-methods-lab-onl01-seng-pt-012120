b75e7439587e06b172588658d12cc24cb9b0d9ce
Dan Foley <dan@dan-foley.com>
2020-01-30 15:12:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,10 +4,9 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  
+  @@count = 0
   @@genres = []
   @@artists = []
-  @@count = 0
   
   def initialize(name, artist, genre)
     @name = name
